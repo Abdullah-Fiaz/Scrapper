be4dd03a4e17e85f29775035fbfe88d61001cdb8
Abdullah-Fiaz <abdullahfiaz271@gmail.com>
2023-12-12 19:13:16 +0500

    Category names and link to websites

---
+++ data.py
@@ -0,0 +1,18 @@
+data = [
+    {
+        "category_name" : "Piston rod cylinders Round cylinders" ,
+        "link": "https://www.festo.com/gb/en/c/products/industrial-automation/actuators/pneumatic-cylinders/piston-rod-cylinder/round-cylinders-id_pim378/"
+    },
+    {
+        "category_name" : "Piston rod cylinders Profile cylinder and tie rod cylinder",
+         "link": "https://www.festo.com/gb/en/c/products/industrial-automation/actuators/pneumatic-cylinders/piston-rod-cylinder/profile-cylinder-and-tie-rod-cylinder-id_pim383/?page=0"
+    },
+    {
+        "category_name": "Piston rod cylinders Compact pneumatic cylinders,short-stroke and flat cylinders ",
+        "link" : "https://www.festo.com/gb/en/c/products/industrial-automation/actuators/pneumatic-cylinders/piston-rod-cylinder/compact-pneumatic-cylinders-short-stroke-and-flat-cylinders-id_pim380/?page=0"
+    },
+    {
+        "category_name": "Mounting attachments for drives Foot mounting",
+        "link" : "https://www.festo.com/gb/en/p/foot-mounting-id_FP_01-50-01-08/?q=%7E%3AfestoSortOrderScored"
+    },
+    ]
